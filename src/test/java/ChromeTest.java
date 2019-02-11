9ce242bb9ee1cccc2e57922517ea1a0dd63247ac
Nigel Charman <nigel.charman.nz@gmail.com>
2019-02-11 21:33:22 +1300

    Initial commit

---
+++ src/test/java/ChromeTest.java
@@ -0,0 +1,43 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class ChromeTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testCheese() throws Exception {
+        WebElement queryBox = driver.findElement(By.name("q99"));
+        queryBox.sendKeys("cheese");
+        queryBox.sendKeys(Keys.RETURN);
+        Thread.sleep(2000);
+    }
+}
---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class GoogleTest {
+
+    @Test
+    public void checkTitle() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        assertEquals("Google", title);
+        driver.quit();
+    }
+}
